82441dc1552a0f237a981fe6dd6e5c8e5bca66a5
hemanth20017 <hemanthvs.vemuri@gmail.com>
2026-01-16 19:57:15 +0530

    first commit

---
+++ learningApp2/Pages/Privacy.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace learningApp2.Pages;
+
+public class PrivacyModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
+
